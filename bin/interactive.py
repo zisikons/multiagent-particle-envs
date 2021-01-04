e78a87a3893e092d0d01c2d238ad3ab47dc7969d
Konstantinos Zisis <zisikons@gmail.com>
2021-01-04 18:15:49 +0100

    Change interactive.py to comply with changes.

--- bin/interactive.py
+++ bin/interactive.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         for i, policy in enumerate(policies):
             act_n.append(policy.action(obs_n[i]))
         # step environment
-        obs_n, reward_n, done_n, _ = env.step(act_n)
+        obs_n, reward_n, done_n, *_ = env.step(act_n)
         # render all agent views
         env.render()
         # display rewards
